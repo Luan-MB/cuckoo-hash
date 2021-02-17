694790e9d5b29c098f5ed0a5d56a071a2b0f40f5
Luan-MB <luanbernardt@hotmail.com>
2021-02-17 14:03:21 -0300

    Comentarios

--- hash.c
+++ hash.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include "hash.h"
 
+// Função que inicia e retorna um array de tamanho size
 int *load_array(int size) {
 
 	int *arr;
@@ -12,25 +13,28 @@ int *load_array(int size) {
 
 	if (!arr)
 		return NULL;
-
+	// Preenche todos os espaços do vetor com -2
 	for (i=0;i<SIZE;i++)
 		arr[i] = -2;
 	
 	return arr;
 }
 
+// Função de endereçamento para o priemrio array do hash
 int h1(int key) {
 
 	int m = SIZE;
 	return (key % m);
 }
 
+// Função de endereçamento para o segundo array do hash
 int h2(int key) {
 	
 	int m = SIZE;
 	return floor(m * (key * 0.9 - floor(key * 0.9)));
 }
 
+// Função que busca chave(key) nos arrays do hash, retorna -1, se não encontrou, e o indice(i), se encontrado
 int search_key(int key, t_hash hash) {
 
 	int i;
@@ -49,18 +53,22 @@ int search_key(int key, t_hash hash) {
 	return -1;
 }
 
+// Função que insere a chave(key) ao hash
 void insert_key(int key, t_hash hash) {
 
 	
 	int i, ki;
-
+	
+	// Se a chave ja existir no hash, retorna-se
 	if ((i = search_key(key,hash)) != -1)
 		return;
 
 	i = h1(key);
-
+	// Tenta-se inserir key em T1[i]
 	if(hash.T1[i] == -2 || hash.T1[i] == -1)
 		hash.T1[i] = key;
+	// Se T1[i] estiver ocupado, calcula-se h2(T1[i]) e move-se T1[i] para T2
+	// Insere-se a chave em T1[i]
 	else {
 		ki = hash.T1[i];
 		hash.T2[h2(ki)] = ki;
@@ -69,24 +77,29 @@ void insert_key(int key, t_hash hash) {
 	return;
 }
 
+// Função que remove a chave(key) do hash
 void remove_key(int key, t_hash hash) {
 
 	int i = search_key(key,hash);
-
+	
+	// Busca-se a chave(key) no hash
+	// Se não encontrou(-1), retorna-se
 	if (i == -1)
 		return;
+	// Se encontrada em T1, a chave é removida e sua posição em T1 é marcada com -1(sinal de exclusão)
 	if (hash.T1[i] == key) {
 		hash.T1[i] = -1;
 		return;
 	}
-	
+	// Se encontrada em T2, a chave é removida e sua posição em T2é marca com -2(sinal de vazio)
 	if (hash.T2[i] == key) {
-		hash.T2[i] = -1;
+		hash.T2[i] = -2;
 		return;
 	}
 	return;
 }
 
+// Função que ordena um vetor(aux) de tamanho len
 void insertion_sort(int *aux, int len) {
 	
 	int i, j, key;
@@ -102,6 +115,7 @@ void insertion_sort(int *aux, int len) {
 	}
 }
 
+// Função que imprime as chaves presentes no hash em ordem crescente
 void print_hash(t_hash hash) {
 
 	int i, k;
@@ -133,7 +147,7 @@ void print_hash(t_hash hash) {
 	free(aux);
 	return;
 }
-
+// Função que desaloca as tabelas de hash da memória
 void free_hash(t_hash hash) {
 
 	free(hash.T1);
